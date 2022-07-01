f509ee5a1ce073a2e740fee24f6e169771ec83cf
sgtest14 <108279074+sgtest14@users.noreply.github.com>
2022-07-01 15:06:23 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = 'https://convairasa.herokuapp.com/webhooks/rest/webhook'
-myobj = {
-"sender":"default",
-"message": "hi"
-}
-x = requests.post(url, json = myobj)
-print(x.text)
